266c1309ee3dfa2f155319dae835a3bad29aad75
justinmajetich <justinmajetich@gmail.com>
2024-12-05 11:43:00 -0500

    Add Playbook workflow and teams types

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,10 +2,18 @@ import {
     ComfyWorkflowGraphData,
     ComfyWorkflowNodeData,
     NodeInputAPIFormat,
+    ComfyWorkflowData,
+    PlaybookWorkflowData,
+    PlaybookTeamData,
+    ComfyWorkflowOutputData,
 } from './types.ts'
 
 export type {
     ComfyWorkflowGraphData,
     ComfyWorkflowNodeData,
     NodeInputAPIFormat,
+    ComfyWorkflowData,
+    PlaybookWorkflowData,
+    PlaybookTeamData,
+    ComfyWorkflowOutputData,
 }
--- packages/core/types.ts
+++ packages/core/types.ts
@@ -1,5 +1,5 @@
 /** 
- * Structural ComfyUI workflow data. Note, workflow data in this 
+ * ComfyUI workflow structural data. Note, workflow data in this 
  * format is retrieved from and saved to our S3 storage bucket.
  */
 export type ComfyWorkflowGraphData = {
@@ -12,6 +12,12 @@ export type ComfyWorkflowGraphData = {
     version: number,
     models?: any[],
 }
+
+/** 
+ * ComfyUI workflow output data. Note, workflow data in this 
+ * format is retrieved from and saved to our S3 storage bucket.
+ */
+export type ComfyWorkflowOutputData = Record<number, { class_type: string, inputs: Record<string, any>}>
 
 /** 
  * Reduction of node data structure in structural ComfyUI workflow data
@@ -28,5 +34,59 @@ export type ComfyWorkflowNodeData = {
  * to pass the values of custom Playbook nodes to backend when a workflow is run.
  * Matches the node structure within Comfy's outputs JSON format.
  */
-export type NodeInputAPIFormat = Record<number, Record<string, string|number|boolean>>
+export type NodeInputAPIFormat = Record<number, Record<string, any>>
 
+/** 
+ * Modeled after a type in the ComfyUI_Frontend repo that provides both 
+ * structural data for the workflow graph, alongside output data which can
+ * be submitted as a prompt to ComfyUI.
+ */
+export type ComfyWorkflowData = {
+    workflow: ComfyWorkflowGraphData,
+    output: ComfyWorkflowOutputData
+}
+
+export interface PlaybookWorkflowData {
+    id: string
+    name: string
+    accounts_ids: string[]
+    runs: string[]
+    owner_id: string
+    team_id: string
+    editing_user_id: string
+    date_created: string
+    date_last_edited: string
+    workflow_url: string
+    workflow_api_url: string
+    s3_file_id: string | null
+    saved_workflows: [[string, string]]
+    canvas_type: number
+    public_url: string
+    public: boolean
+    is_template: boolean
+    outputs_urls: string[]
+    gallery_outputs_urls: string[]
+    gpu_usage_total: number
+    gpu_usage_billing_period: number
+}
+
+export interface PlaybookTeamData {
+    id: string
+    name: string
+    owner_id: string
+    api_key: string
+    date_created: string
+    date_last_login: string
+    billing_date: string
+    runs: [string]
+    members: [string]
+    subscription_tier: string
+    custom_monthly_rate: number
+    custom_free_credit: number
+    gpu_usage_total: number
+    gpu_usage_billing_period: number
+    billing_overage: boolean
+    projects: [string]
+    models_urls: [string]
+    modal_app_id: string
+}
